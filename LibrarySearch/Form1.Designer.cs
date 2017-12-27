cf15ac53c07798e4318b7e4e3e99d55f66543b43
15926138059 <34852713+15926138059@users.noreply.github.com>
2017-12-27 16:17:10 +0800

    搜索

--- LibrarySearch/Form1.Designer.cs
+++ LibrarySearch/Form1.Designer.cs
@@ -28,20 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bookname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.author = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pressdate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.press = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Font = new System.Drawing.Font("新宋体", 12F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(399, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Font = new System.Drawing.Font("新宋体", 12F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(129, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 24);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.DropDownWidth = 80;
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "图书名",
+            "作者名",
+            "ISBN",
+            "主题词",
+            "出版社",
+            "索书号"});
+            this.comboBox1.Location = new System.Drawing.Point(50, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.bookname,
+            this.author,
+            this.pressdate,
+            this.press});
+            this.dataGridView1.Location = new System.Drawing.Point(41, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(436, 446);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // bookname
+            // 
+            this.bookname.HeaderText = "书名";
+            this.bookname.Name = "bookname";
+            this.bookname.ReadOnly = true;
+            // 
+            // author
+            // 
+            this.author.HeaderText = "作者";
+            this.author.Name = "author";
+            this.author.ReadOnly = true;
+            // 
+            // pressdate
+            // 
+            this.pressdate.HeaderText = "出版时间";
+            this.pressdate.Name = "pressdate";
+            this.pressdate.ReadOnly = true;
+            // 
+            // press
+            // 
+            this.press.HeaderText = "出版社";
+            this.press.Name = "press";
+            this.press.ReadOnly = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(513, 548);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("新宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Name = "Form1";
             this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn author;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pressdate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn press;
     }
 }
 
--- LibrarySearch/Form1.cs
+++ LibrarySearch/Form1.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace LibrarySearch
 {
     public partial class Form1 : Form
     {
+        string a = null;
+        int  b;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bookname, author, pressdate, press;
+            SqlConnection con = Util.SqlConnection();
+            con.Open();
+            a = textBox1.Text;
+            b = comboBox1.SelectedIndex;
+            using (SqlCommand cmd = con.CreateCommand())
+            {
+                switch (b)
+                {
+                    case 0: cmd.CommandText = "select bookName,author,publishingDate,pressName from BookClassTable,press where BookClassTable.pressID=press.pressID and BookClassTable.bookName = @a_in;"; break; // 图书名
+                    case 1: cmd.CommandText = "select bookName,author,publishingDate,pressName from BookClassTable,press where BookClassTable.pressID=press.pressID and BookClassTable.author = @a_in;"; break; //作者名
+                    case 2: cmd.CommandText = "select bookName,author,publishingDate,pressName from BookClassTable,press where BookClassTable.pressID=press.pressID and BookClassTable.ISBN = @a_in;"; break; //ISBN
+                    case 3: cmd.CommandText = "select bookName,author,publishingDate,pressName from BookClassTable,press,book_subjectsTable,subjectsTable where BookClassTable.pressID=press.pressID and BookClassTable.ISBN=book_subjectssTable.ISBN and book_subjectsTable.subjectID=subjectsTable.subjectID and subjectsTable.subjectWord = @a_in;"; break; //主题词
+                    case 4: cmd.CommandText = "select bookName,author,publishingDate,pressName from BookClassTable,press where BookClassTable.pressID=press.pressID and press.pressName = @a_in;"; break; //出版社
+                    case 5: cmd.CommandText = "select bookName,author,publishingDate,pressName from BookClassTable,press,bookTable where BookClassTable.pressID=press.pressID and BookClassTable.ISBN=bookTable.ISBN and bookTable.bookID = @a_in;"; break; //索书号
+                }
+                cmd.Parameters.Add(new SqlParameter("a_in", a));
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    bookname = reader.GetString(0);
+                    author = reader.GetString(1);
+                    pressdate = reader.GetDateTime(2).Year.ToString();
+                    press = reader.GetString(3);
+                    dataGridView1.Rows.Add(bookname, author,pressdate,press);
+                    dataGridView1.Visible = true;
+                }
+
+            }
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
